4a78034cd4176634e56f2b35bf69432fdf706633
Koroliuk <koroliuk.yaroslav@gmail.com>
2024-02-25 17:02:37 +0200

    Lab2: Fix instance init script

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,7 +64,7 @@ resource "aws_instance" "kpi-lab2-server" {
 
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-    sudo docker run -p 80:80 --restart always --name lab2 pienskoi/webapp:latest
+    sudo docker run -d -p 80:80 --restart always --name lab2 pienskoi/webapp:latest
     sudo docker run -d --restart always --name watchtower -v /var/run/docker.sock:/var/run/docker.sock -e WATCHTOWER_POLL_INTERVAL=60 containrrr/watchtower
 
     EOF
